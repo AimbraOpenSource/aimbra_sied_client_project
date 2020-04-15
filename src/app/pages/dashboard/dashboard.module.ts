76718fdfb4a88f16969bab149540f6f7d32b5f5c
Thiago Cunha <thiagosantoscunha@outlook.com>
2020-04-15 02:12:41 -0300

    API de interface com youtube embeded

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { BootstrapAlertModule } from 'src/app/components/alerts/bootstrap-alert/bootstrap-alert.module';
+import {MaterialCardModule} from "../../components/cards/material-card/material-card.module";
+import {MatButtonModule} from "@angular/material/button";
+import {YoutubeVideoModule} from "../../components/youtube-video/youtube-video.module";
 
 
 const routes: Routes = [
@@ -18,7 +21,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    BootstrapAlertModule
+    BootstrapAlertModule,
+    MatButtonModule,
+    MaterialCardModule,
+    YoutubeVideoModule
   ],
   exports: [DashboardComponent],
 })
