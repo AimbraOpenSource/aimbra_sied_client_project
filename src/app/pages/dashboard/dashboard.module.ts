b586b436889b08c669907b07f5689a2c35e53131
Thiago Cunha <thiagosantoscunha@outlook.com>
2020-04-15 01:00:55 -0300

    Criação de um bootstrap alert

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { BootstrapAlertModule } from 'src/app/components/alerts/bootstrap-alert/bootstrap-alert.module';
 
 
 const routes: Routes = [
@@ -17,6 +18,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    BootstrapAlertModule
   ],
   exports: [DashboardComponent],
 })
